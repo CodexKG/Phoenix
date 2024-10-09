8c5a6f1096ada19948dce26ece8a41c80e1f4c8c
Toktorov <ktoktorov2005@gmail.com>
2024-10-09 10:55:14 +0600

    add new models and auth to panel

--- apps/queenbee/urls.py
+++ apps/queenbee/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 
 from apps.queenbee.moduls.index import crm_index
 from apps.queenbee.moduls import kanban
+from apps.queenbee.moduls import users
 
 urlpatterns = [
     path('', crm_index, name="crm_index"),
+
+    #kanban
     path('kanban/', kanban.crm_kanban_index, name="crm_kanban_index"),
     path('kanban/<int:id>/', kanban.crm_kanban_detail, name="crm_kanban_detail"),
     path('kanban/create/<int:board_id>/', kanban.crm_add_list, name="crm_add_list"),
@@ -13,4 +16,7 @@ urlpatterns = [
     path('kanban/edit_board/<int:board_id>/', kanban.crm_edit_board, name='crm_edit_board'),
     path('kanban/list/<int:list_id>/add_card/', kanban.crm_add_card, name='crm_add_card'),
     path('kanban/update_card_positions/', kanban.crm_update_card_positions, name='update_card_positions'),
+
+    #users
+    path('login/', users.crm_login, name="crm_login"),
 ]
