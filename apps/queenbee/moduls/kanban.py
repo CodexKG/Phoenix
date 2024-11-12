38e060b9428a20828cfd73e204d8a79ac42e4775
Kulmamatov Zalkar <kulmamatov.zalkar@gmail.com>
2024-11-13 01:57:47 +0600

    Test

--- apps/queenbee/moduls/kanban.py
+++ apps/queenbee/moduls/kanban.py
@@ -3,7 +3,8 @@ from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
-import logging, json
+import logging
+import json
 
 from apps.kanban import models, forms
 from apps.queenbee.permissions import permission_required
@@ -11,22 +12,25 @@ from apps.erp.models import Employee
 
 logger = logging.getLogger(__name__)
 
+
 @staff_member_required(login_url='/admin/login')
 @permission_required('crm_kanban_index', 'Просмотр раздела Задачник')
 def crm_kanban_index(request):
     boards = models.Board.objects.all().order_by('-created_at')
     form = forms.BoardForm()
-    
+
     return render(request, 'queenbee/kanban/index.html', {
         'boards': boards,
         'form': form
     })
 
+
 @staff_member_required(login_url='/admin/login/')
 @permission_required('crm_kanban_detail', 'Детальный просмотр раздела Задачник')
 def crm_kanban_detail(request, id):
     board = get_object_or_404(models.Board, id=id)
-    lists = models.List.objects.filter(board=board).prefetch_related('card_lists')
+    lists = models.List.objects.filter(
+        board=board).prefetch_related('card_lists')
     employees = Employee.objects.all()
 
     # Формы для добавления карточки и вложений
@@ -41,16 +45,18 @@ def crm_kanban_detail(request, id):
         'attachment_formset': attachment_formset,
     })
 
+
 def crm_add_list(request, board_id):
-    logger.info("Получен запрос на добавление списка")  # Логируем начало запроса
-    
+    # Логируем начало запроса
+    logger.info("Получен запрос на добавление списка")
+
     if request.method == 'POST':
         logger.info("Метод запроса POST, пытаемся получить доску")
         board = get_object_or_404(models.Board, id=board_id)
-        
+
         logger.info("Доска найдена: %s", board.title)
         form = forms.ListForm(request.POST)
-        
+
         if form.is_valid():
             logger.info("Форма валидна, продолжаем создание списка")
             new_list = form.save(commit=False)
@@ -60,10 +66,10 @@ def crm_add_list(request, board_id):
             # Получаем последнюю позицию
             last_position = models.List.objects.filter(board=board).count() + 1
             new_list.position = last_position
-            
+
             logger.info("Присвоена позиция: %d", last_position)
             new_list.save()
-            
+
             logger.info("Список успешно сохранен с ID: %d", new_list.id)
             return JsonResponse({
                 'title': new_list.title,
@@ -73,14 +79,16 @@ def crm_add_list(request, board_id):
             })
         else:
             logger.error("Ошибка в форме: %s", form.errors)
-    
+
     logger.error("Ошибка создания списка")
     return JsonResponse({'error': 'Ошибка создания списка'}, status=400)
 
+
 @staff_member_required(login_url='/admin/login/')
 def crm_add_board(request):
-    logger.info("Получен запрос на добавление новой доски")  # Логируем начало запроса
-    
+    # Логируем начало запроса
+    logger.info("Получен запрос на добавление новой доски")
+
     if request.method == 'POST':
         form = forms.BoardForm(request.POST)
         if form.is_valid():
@@ -88,7 +96,7 @@ def crm_add_board(request):
             new_board = form.save(commit=False)
             new_board.owner = request.user
             new_board.save()
-            
+
             logger.info("Доска успешно сохранена с ID: %d", new_board.id)
             return JsonResponse({
                 'title': new_board.title,
@@ -98,9 +106,10 @@ def crm_add_board(request):
         else:
             logger.error("Ошибка в форме: %s", form.errors)
             return JsonResponse({'error': 'Ошибка создания доски'}, status=400)
-    
+
     logger.error("Метод запроса не POST")
     return JsonResponse({'error': 'Метод запроса не POST'}, status=400)
+
 
 def crm_delete_board(request, board_id):
     if request.method == 'POST':
@@ -111,6 +120,7 @@ def crm_delete_board(request, board_id):
         except models.Board.DoesNotExist:
             return JsonResponse({'success': False, 'error': 'Доска не найдена'})
     return JsonResponse({'success': False, 'error': 'Неверный запрос'})
+
 
 def crm_edit_board(request, board_id):
     if request.method == 'POST':
@@ -128,11 +138,13 @@ def crm_edit_board(request, board_id):
             return JsonResponse({'success': False, 'error': 'Доска не найдена'})
     return JsonResponse({'success': False, 'error': 'Неверный запрос'})
 
-#Карточки
+# Карточки
 # Представление для создания карточки
+
+
 def crm_add_card(request, list_id):
     list_obj = get_object_or_404(models.List, id=list_id)
-    
+
     if request.method == 'POST':
         form = forms.CardForm(request.POST, request.FILES)
         formset = forms.AttachmentInlineFormset(request.POST, request.FILES)
@@ -154,10 +166,10 @@ def crm_add_card(request, list_id):
                 attachment.save()
             formset.save()
 
-            return JsonResponse({'success': True, 'card_title': card.title, 'card_id': card.id})
+            return JsonResponse({'success': True, 'card_title': card.title, 'card_description': card.description, 'card_id': card.id})
         else:
             return JsonResponse({'success': False, 'errors': form.errors})
-    
+
     else:
         form = forms.CardForm()
         formset = forms.AttachmentInlineFormset()
@@ -166,14 +178,15 @@ def crm_add_card(request, list_id):
         'form': form,
         'formset': formset,
     })
-    
+
+
 @csrf_exempt
 def crm_update_card_positions(request):
     if request.method == "POST":
         data = json.loads(request.body)
         card_ids = data.get('cardIds', [])
         new_list_id = data.get('listId', None)
-        
+
         if not new_list_id:
             return JsonResponse({'success': False, 'error': 'New list ID is required'})
 
@@ -191,10 +204,11 @@ def crm_update_card_positions(request):
                 card.save()
             except models.Card.DoesNotExist:
                 return JsonResponse({'success': False, 'error': f'Card with ID {card_id} not found'})
-            
+
         return JsonResponse({'success': True})
-    
+
     return JsonResponse({'success': False, 'error': 'Invalid request method'})
+
 
 @login_required(login_url='/admin/login/')
 def crm_delete_card(request, card_id):
@@ -206,3 +220,41 @@ def crm_delete_card(request, card_id):
         return JsonResponse({'success': False, 'error': 'Карточка не найдена'})
     except Exception as e:
         return JsonResponse({'success': False, 'error': str(e)})
+
+
+@csrf_exempt
+def crm_update_card_positions(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+        card_ids = data.get('cardIds', [])
+        new_list_id = data.get('listId', None)
+
+        if not new_list_id:
+            return JsonResponse({'success': False, 'error': 'New list ID is required'})
+
+        try:
+            new_list = models.List.objects.get(id=new_list_id)
+        except models.List.DoesNotExist:
+            return JsonResponse({'success': False, 'error': 'List not found'})
+
+        # Обновляем карточки в новом порядке
+        for index, card_id in enumerate(card_ids):
+            try:
+                if card_id is None:
+                    continue  # Игнорируем пустые значения
+                card = models.Card.objects.get(id=card_id)
+                card.position = index
+                card.list = new_list
+                card.save()
+            except models.Card.DoesNotExist:
+                logger.error(f'Карточка с ID {card_id} не найдена')
+                continue
+
+        # Создаем словарь с количеством задач для каждого списка
+        task_counts = {}
+        for kanban_list in models.List.objects.filter(board=new_list.board):
+            task_counts[kanban_list.id] = kanban_list.card_lists.count()
+
+        return JsonResponse({'success': True, 'taskCounts': task_counts})
+
+    return JsonResponse({'success': False, 'error': 'Invalid request method'})
