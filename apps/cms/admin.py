7f47e950343e53ea7cf54ed3ac0830ebad8c016d
Toktorov <ktoktorov2005@gmail.com>
2024-11-14 13:31:33 +0600

    start work with cms 30%

--- apps/cms/admin.py
+++ apps/cms/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
 
-from apps.cms.models import Visit
+from apps.cms.models import Visit, Partners, Service, FAQ
 
 # Register your models here.
 @admin.register(Visit)
 class VisitAdmin(admin.ModelAdmin):
     list_display = ('ip_address', 'user_agent', 'url_path', 'timestamp')
+
+@admin.register(Partners)
+class PartnersAdmin(admin.ModelAdmin):
+    list_display = ('user', 'title', 'partners_url', 'created')
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('user', 'title', 'created')
+
+@admin.register(FAQ)
+class FAQAdmin(admin.ModelAdmin):
+    list_display = ('user', 'question')
