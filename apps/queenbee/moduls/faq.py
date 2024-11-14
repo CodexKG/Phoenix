c9657bcb47e04b7a4ac0f968cbfdf63a822ed91e
Toktorov <ktoktorov2005@gmail.com>
2024-11-14 14:24:24 +0600

    kanban changes

---
+++ apps/queenbee/moduls/faq.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.admin.views.decorators import staff_member_required
+from django.contrib.admin.utils import label_for_field
+from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
+from django.contrib import admin
+import traceback, logging, json
+
+from apps.cms.models import FAQ
+from apps.cms.forms import FAQForm
+from apps.queenbee.permissions import permission_required
